eb135183e9a866e9f201921ac64c36bf491bc311
bibinvasudev@gmail.com <bibinvasudev@gmail.com>
2023-05-04 23:57:49 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -8,8 +8,7 @@ def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
         
-#os.environ["OPENAI_API_KEY"] = open_file('key_openai.txt')
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "Your-Openai-Key"
 
 def construct_index(directory_path):
   # set maximum input size
@@ -20,18 +19,12 @@ def construct_index(directory_path):
   max_chunk_overlap = 20
   # set chunk size limit
   chunk_size_limit = 600
-
   prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
   # define LLM
   llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003", max_tokens=num_outputs))
-  
   documents = SimpleDirectoryReader(directory_path).load_data()
-  
   index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-  
   index.save_to_disk('index.json')
-  
   return index
   
 def ask_bot(input_index = 'index.json'):
@@ -41,7 +34,6 @@ def ask_bot(input_index = 'index.json'):
     response = index.query(query, response_mode="compact")
     print ("\nJJ: " + response.response + "\n")
 
-    
 if __name__ == '__main__':
     index = construct_index("data/")
     ask_bot('index.json')
